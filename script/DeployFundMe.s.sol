feea2457400a3099ee3e6e36123fcadc0e24c7ee
Ara-miri <aramiri.trade@gmail.com>
2025-02-28 19:49:26 +0330

    renamed FundToMe contract name to FundMe

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/Test.sol";
-import {FundToMe} from "../src/FundToMe.sol";
+import {FundMe} from "../src/FundMe.sol";
 import {MockV3Aggregator} from "../test/Mocks/MockV3Aggregator.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployFundToMe is Script {
-  function run() external returns (FundToMe) {
-    HelperConfig helperConfig = new HelperConfig();
-    address priceFeed = helperConfig.activeNetworkConfig();
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address priceFeed = helperConfig.activeNetworkConfig();
 
-    vm.startBroadcast(); // Start sending transactions
+        vm.startBroadcast(); // Start sending transactions
 
-    // 🔹 Deploy FundMe contract with the selected price feed
-    FundToMe fundMe = new FundToMe(priceFeed);
-    vm.stopBroadcast(); // Stop sending transactions
-    return fundMe;
-  }
+        // Deploy FundMe contract with the selected price feed
+        FundMe fundMe = new FundMe(priceFeed);
+        vm.stopBroadcast(); // Stop sending transactions
+        return fundMe;
+    }
 }
