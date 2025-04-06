ded282c09d8132b718c4cce11609807a96171c57
Ara-miri <aramiri.trade@gmail.com>
2025-04-06 13:40:38 +0330

    Separated tests from deployment script to avoid extra complications

--- Contract/test/ProxyFundMe.t.sol
+++ Contract/test/ProxyFundMe.t.sol
@@ -1,26 +1,36 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
 import {FundMeV2} from "../src/FundMeV2.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {TransparentUpgradeableProxy, ITransparentUpgradeableProxy, ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract ProxyFundMeTest is Test {
-    FundMe public fundMeV1;
     address upgradeAdmin;
     FundMeV2 public fundMeV2;
-    DeployFundMe public deployFundMe;
+    FundMe public fundMeV1;
     TransparentUpgradeableProxy public proxy;
     address proxyAdmin;
+    HelperConfig public helperConfig;
+    address priceFeedAddress;
 
     address public constant USER = address(1);
     uint256 public constant SEND_VALUE = 1 ether;
     uint256 public constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
-        deployFundMe = new DeployFundMe();
-        (fundMeV1, proxy, upgradeAdmin) = deployFundMe.run();
+        helperConfig = new HelperConfig();
+        priceFeedAddress = helperConfig.activeNetworkConfig();
+        fundMeV2 = new FundMeV2();
+        proxy = new TransparentUpgradeableProxy(
+            address(fundMeV2),
+            msg.sender,
+            abi.encodeWithSelector(FundMe.initialize.selector, priceFeedAddress) // Initializer data
+        );
+        fundMeV2 = FundMeV2(payable(address(proxy)));
+        upgradeAdmin = msg.sender;
+
         /**
          * @dev Storage slot with the admin of the contract.
          * This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1.
@@ -31,28 +41,6 @@ contract ProxyFundMeTest is Test {
         proxyAdmin = address(uint160(uint256(data)));
 
         vm.deal(USER, 10 ether); // We give the fake user a starting balance of ETH
-    }
-
-    modifier UpgradeToV2() {
-        require(
-            ProxyAdmin(proxyAdmin).owner() == upgradeAdmin,
-            "Admin mismatch"
-        );
-        vm.startPrank(upgradeAdmin);
-        fundMeV2 = new FundMeV2();
-        /**
-         * @notice proxyAdmin is not the upgradeAdmin.
-         * @notice upgradeAdmin is an EOA that deploys the proxyAdmin
-         */
-        // Upgrade the proxy to the new implementation using the proxyAdmin
-        ProxyAdmin(proxyAdmin).upgradeAndCall(
-            ITransparentUpgradeableProxy(address(proxy)),
-            address(fundMeV2),
-            ""
-        );
-        fundMeV2 = FundMeV2(payable(address(proxy)));
-        vm.stopPrank();
-        _;
     }
 
     /**
@@ -77,27 +65,11 @@ contract ProxyFundMeTest is Test {
         );
     }
 
-    function test_GetCorrectPriceFeedAddressAndItsVersionThroughProxy()
-        public
-        UpgradeToV2
-    {
-        // Verify price feed is set correctly
-        address getPriceFeedAddressV1 = fundMeV1.getPriceFeed();
-        address getPriceFeedAddressV2 = fundMeV2.getPriceFeed();
-        assertEq(getPriceFeedAddressV1, getPriceFeedAddressV2);
-
-        // Check version matches network expectations
-        /** @dev priceFeed version for mainnet is 6 and for others is 4 */
-        uint256 versionV1 = fundMeV1.getVersion();
-        uint256 versionV2 = fundMeV2.getVersion();
-        assertEq(versionV1, versionV2);
-    }
-
-    function test_UserFundsPersistAfterV1ToV2Upgrade() public UpgradeToV2 {
+    function test_UserFundsPersistAfterV1ToV2Upgrade() public {
         // 1. Set state in V1
         vm.startPrank(USER);
-        fundMeV1.fund{value: SEND_VALUE}();
-        uint256 initialAmount = fundMeV1.getAddressToAmountFunded(USER);
+        fundMeV2.fund{value: SEND_VALUE}();
+        uint256 initialAmount = fundMeV2.getAddressToAmountFunded(USER);
         vm.stopPrank();
         // 2. Verify state persists in V2
         assertEq(fundMeV2.getAddressToAmountFunded(USER), initialAmount);
@@ -106,8 +78,8 @@ contract ProxyFundMeTest is Test {
     function test_UserWithdrawalFunctionalityAfterV1ToV2Upgrade() public {
         // Used previous test to upgrade to V2 and fund the contract
         test_UserFundsPersistAfterV1ToV2Upgrade();
-        uint256 contractBalance = address(fundMeV1).balance;
-        uint256 userBalanceInContract = fundMeV1.getAddressToAmountFunded(USER);
+        uint256 contractBalance = address(fundMeV2).balance;
+        uint256 userBalanceInContract = fundMeV2.getAddressToAmountFunded(USER);
         // User funds 1 ether to contract, so contract balance and user balance in contract should be equal to 1 ether
         assertEq(contractBalance, userBalanceInContract);
 
@@ -127,7 +99,7 @@ contract ProxyFundMeTest is Test {
         );
     }
 
-    function test_FundMeV2NewFunction() public UpgradeToV2 {
+    function test_FundMeV2NewFunction() public {
         uint256 testValue = 42;
         vm.prank(USER);
         // setNewValue is a new function in FundMeV2
